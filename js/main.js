16e22efce2becbbccbbdae58d9730b922f659840
Andy Locascio <andy.locascio@gmail.com>
2014-02-19 15:03:59 -0500

    more styles

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// $(function() {
+// 	$header = $('header');
+
+// 	$(window).on("scroll", function() {
+// 		var scrollTop = $(window).scrollTop();
+// 		if ((scrollTop < 5)) {
+// 			$header.removeClass('small');
+// 			$header.animate({height: "80px"}, 100);
+// 		}
+// 		else if (!$header.hasClass('small')) {
+// 			$header.addClass('small');
+// 			$header.animate({height: "60px"}, 100);
+// 		}
+// 	});
+// });
