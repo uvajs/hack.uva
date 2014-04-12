7f0575c9d016369fac08af256787ff219bb87b62
jasdev <js2wt@virginia.edu>
2014-04-11 21:40:29 -0400

    twilio messagine

---
+++ scripts/twilio_runner.py
@@ -0,0 +1,24 @@
+import os
+from twilio import rest
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+account_sid = os.environ['TWILIO_SID']
+auth_token = os.environ['TWILIO_TOKEN']
+
+client = rest.TwilioRestClient(account_sid, auth_token)
+
+
+def send_message(text):
+    f = open('phones.csv')
+    numbers = f.read().split('\r')
+    for number in numbers:
+        message = client.sms.messages.create(body=text,
+            to=number,
+            from_="+14438981316")
+
+
+if __name__ == "__main__":
+    pass
